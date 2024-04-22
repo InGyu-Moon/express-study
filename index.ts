7f494ff7cd684280e84cd820a083f294aa956609
InGyu Moon <ansdlsrb1121@naver.com>
2024-04-22 23:21:39 +0900

    0422 멘토링

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ import userRouter from './src/controllers/user';
 const app = express()
 const port = 3000
 
+//jsx가 뭔지
+// cors 에러 해결하기
 
 
 declare module 'express-session' {
@@ -29,9 +31,8 @@ app.use(session({
 }))
 app.use('/user', userRouter);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
 mongoose.connect('mongodb+srv://root:1234@cluster0.1pwaaf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
     console.log('success');
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, response } from 'express';
 import { UserModel,User } from '../models/user';
 
 const router = express.Router();
@@ -94,6 +94,42 @@ router.get('/session/logout',async(req:Request,res:Response)=>{
     }
 });
 
+router.get('/test/get',async(req:Request,res:Response)=>{
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users',{
+            method:"GET",
+        })
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            res.status(200).json({data:data});
+        }
+    }catch(error){
+        console.log(error);
+    }
+})
+router.post('/test/post',async(req:Request,res:Response)=>{
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+            method:"POST",
+            body:JSON.stringify({
+                title:"foo",
+                body:"bar",
+                userId:1,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            res.status(200).json({data:data});
+        }
+    }catch(error){
+        console.log(error);
+    }
+})
 
 
 export default router;
